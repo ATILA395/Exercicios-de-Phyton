ec1407008f9dff00653e027c01b2dd10ccde50f2
Átila <atilasantosdemello@gmail.com>
2024-10-07 19:31:44 -0300

    Create Gráfico.py

---
+++ Gráfico.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Dados de exemplo
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Criando o gráfico de dispersão
+plt.scatter(x, y)
+
+# Adicionando títulos e rótulos
+plt.title('Gráfico de Dispersão de Exemplo')
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+
+# Exibindo o gráfico
+plt.show()
